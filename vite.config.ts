294bbaa8399e2d80dc7880113a86649f2be2ef90
amalvivek <avivek99@gmail.com>
2024-03-03 15:18:17 +0530

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,48 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss'
 import vitePluginSingleSpa from 'vite-plugin-single-spa'
 import vercel from 'vite-plugin-vercel'
-import { viteEnv } from 'vite-plugin-single-spa/ex'
-
-let base = 'http://localhost:3004'
-if (viteEnv.built) base = 'https://hype-client.vercel.app'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ['spline-viewer'].includes(tag)
+export default defineConfig(({ command }) => {
+  let base = 'http://localhost:3004'
+  if (command === 'build') base = 'https://hype-client.vercel.app'
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => ['spline-viewer'].includes(tag)
+          }
         }
+      }),
+      vitePluginSingleSpa({
+        type: 'mife',
+        serverPort: 3004
+      }),
+      vercel()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }),
-    vitePluginSingleSpa({
-      type: 'mife',
-      serverPort: 3004
-    }),
-    vercel()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    css: {
+      postcss: {
+        plugins: [tailwindcss]
+      }
+    },
+    server: {
+      hmr: false
+    },
+    base: base,
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url))
     }
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss]
-    }
-  },
-  server: {
-    hmr: false
-  },
-  base: base
+  }
 })
